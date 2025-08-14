27bdb9b53782a547dfc9cbfd8c4ff8a9b16cfec5
OTEBS <beto@example.com>
2025-08-14 21:27:05 +0800

    4_Basic_Conditional_Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-// Import the Student component to use in this App component
-import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 // Define the main App component
 function App() {
   return (
     <>
-      {/* Render multiple Student components with different props */}
-      <Student name="Sponebob" age={30} isStudent={true} />
-      <Student name="Partick" age={42} isStudent={false} />
-      <Student name="Squidward" age={50} isStudent={false} />
-      <Student name="Sandy" age={27} isStudent={true} />
-      {/* Render a Student component without props, defaults will be used */}
-      <Student />
+      <UserGreeting isLoggedIn={true} username="SERVER STATUS" />
     </>
   );
 }
 
-// Export the App component as the default export
 export default App;
