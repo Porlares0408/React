8241f22e1432e3e60943376b56a7b916136e23d4
OTEBS <beto@example.com>
2025-08-14 19:40:23 +0800

    1_Creating_Basic_React

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>&Copy; {new Date().getFullYear()} My FirtsWeb React</p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <h1>My First Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#"></a>HOME
+          </li>
+          <li>
+            <a href="#"></a>CONTACT
+          </li>
+          <li>
+            <a href="#"></a>SERVICE
+          </li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+}
+export default Header;
