bfd20ccc516a0e38221c6873975fe15f48244b9e
OTEBS <beto@example.com>
2025-08-14 19:52:03 +0800

    3_prop-types and default

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Card from "./Card";
+// Import the Student component to use in this App component
+import Student from "./Student";
+
+// Define the main App component
 function App() {
   return (
     <>
-      <Card />
+      {/* Render multiple Student components with different props */}
+      <Student name="Sponebob" age={30} isStudent={true} />
+      <Student name="Partick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      {/* Render a Student component without props, defaults will be used */}
+      <Student />
     </>
   );
 }
 
+// Export the App component as the default export
 export default App;
