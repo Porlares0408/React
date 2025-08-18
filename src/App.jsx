8c9b6d8d32ba58e6e0799b0e9eb85c1f2fa70cf1
OTEBS <beto@example.com>
2025-08-18 22:07:34 +0800

    5_Rendering_List

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import UserGreeting from "./UserGreeting";
+import List from "./List";
 
-// Define the main App component
 function App() {
+  const fruits = [
+    { id: 1, name: "Apple", Calories: 95 },
+    { id: 2, name: "Orange", Calories: 45 },
+    { id: 3, name: "Banana", Calories: 105 },
+    { id: 4, name: "Coconut", Calories: 159 },
+    { id: 5, name: "Pineapple", Calories: 37 },
+  ];
+
+  const vegetables = [
+    { id: 1, name: "Carrot", Calories: 25 },
+    { id: 2, name: "Broccoli", Calories: 55 },
+    { id: 3, name: "Spinach", Calories: 20 },
+    { id: 4, name: "Pepper", Calories: 30 },
+    { id: 5, name: "Tomato", Calories: 18 },
+  ];
+
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="SERVER STATUS" />
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
     </>
   );
 }
